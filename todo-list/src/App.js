67a825a291d0660913eef825f3170d38b08382bd
TrungVu <trungvu1706@gmail.com>
2021-01-06 14:45:54 +0700

    remix code

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = ["Go to maket", "buy food", "make dinner"];
+  const todoList = todos.map((item) => <li>{item}</li>);
   return (
     <div className="App">
-      <ul class="todo-List">
-          <li>Go to maket</li>
-          <li>Buy food</li>
-          <li>Make dinner</li>
-      </ul>
+     <ul>{todoList}</ul>
     </div>
   );
 }
