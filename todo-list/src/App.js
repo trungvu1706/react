1b4b3f78bef448f11b2ba995b0b1ba94b1c846a6
TrungVu <trungvu1706@gmail.com>
2021-01-06 14:18:54 +0700

    create react app

---
+++ todo-list/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul class="todo-List">
+          <li>Go to maket</li>
+          <li>Buy food</li>
+          <li>Make dinner</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
