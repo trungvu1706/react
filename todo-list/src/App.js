729323fd509bb4819cf4df057962031773aa4afa
TrungVu <trungvu1706@gmail.com>
2021-01-06 14:56:32 +0700

    improve code in file app.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   const todos = ["Go to maket", "buy food", "make dinner"];
-  const todoList = todos.map((item) => <li>{item}</li>);
+  // const todoList = todos.map((item) => <li>{item}</li>);
+  const renderList = () =>{
+    return todos.map((item)=> <li>{item}</li>);
+  }
   return (
     <div className="App">
-     <ul>{todoList}</ul>
+     <ul>{renderList()}</ul>
     </div>
   );
 }
